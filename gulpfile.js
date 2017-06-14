6e2aaf2934c48500883a583e4d3f6dd2fc6ce4df
huyenltnguyen <huyenltnguyen@outlook.com>
2017-06-15 00:26:17 +0700

    jshint-stylish

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var prefix = require("gulp-autoprefixer");
 var concatCSS = require("gulp-concat-css");
 var sass = require("gulp-sass");
 var jshint = require("gulp-jshint");
+var jshintStylish = require("jshint-stylish")
 
 // handles gulp errors
 function handleErrors(error) {
@@ -49,7 +50,7 @@ gulp.task('scripts', function() {
 	console.log("starting scripts");
 	gulp.src("public/js/main.js")
 		.pipe(jshint())
-		.pipe(jshint.reporter("default"))
+		.pipe(jshint.reporter(jshintStylish))
 		.pipe(jshint.reporter("fail"))
 		.on("error", function() {
 			this.emit("end");	
